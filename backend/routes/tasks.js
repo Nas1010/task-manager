5a6939a0e9440b3f2d96ad26fcaa7a0ec08ec2aa
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-04-07 21:50:20 +0100

    Defined routes in tasks.js

---
+++ backend/routes/tasks.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { 
+  createTask, 
+  getTasks, 
+  getTaskById, 
+  updateTaskStatus, 
+  deleteTask 
+} from '../controllers/taskController.js';
+
+const router = express.Router();
+
+router.post('/', createTask);
+
+router.get('/', getTasks);
+
+router.get('/:id', getTaskById);
+
+router.put('/:id/status', updateTaskStatus);
+
+router.delete('/:id', deleteTask);
+
+export default router;
