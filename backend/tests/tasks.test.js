1444e14a3bc64fce0f16d1e8fdd87ae721f0a04e
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-04-08 20:23:38 +0100

    Test for PUT and DELETE functionality

--- backend/tests/tasks.test.js
+++ backend/tests/tasks.test.js
@@ -3,6 +3,8 @@ import app from './server.js'
 import { describe, it } from 'vitest';
 
 describe('Task API', () => {
+    let taskId;
+
     //Test for Get all tasks
     it('should return all tasks', async () => {
         const res = await request(app).get('/api/tasks');
@@ -19,21 +21,43 @@ describe('Task API', () => {
         
         expect(res.status).toBe(200);  
         expect(res.body).toHaveProperty('id', validId);  
+        expect(res.body).toHaveProperty('title');
+        expect(res.body).toHaveProperty('description');
       });
       
     //Test for Post request
-    it('should create a task'), async () => {
+    it('should create a task', async () => { 
         const res = await request(app)
-        .post('/api/tasks')
-        .send({
-          title: 'Testing POST request',
-          description: 'Testing task creation',
-          status: 'pending',
-          due_date: '2025-05-01T12:00:00',
-        });
+            .post('/api/tasks')
+            .send({
+                title: 'Testing POST request',
+                description: 'Testing task creation',
+                status: 'pending',
+                due_date: '2025-05-01T12:00:00',
+            });
   
-      expect(res.status).toBe(201);
-      expect(res.body.title).toBe('Testing POST request');
-      taskId = res.body.id;
-    }
+        expect(res.status).toBe(201);
+        expect(res.body.title).toBe('Testing POST request');
+        taskId = res.body.id;  
+    });
+
+    //Test for PUT request
+
+    it('should update the task status', async () => {
+        const res = await request(app)
+          .put(`/api/tasks/${taskId}/status`)
+          .send({ status: 'completed' });
+    
+        expect(res.status).toBe(200);
+        expect(res.body.status).toBe('completed');
+    });
+    
+    // Test for DELETE request
+    it('should delete a task', async () => {
+        const res = await request(app)
+            .delete(`/api/tasks/${taskId}`);
+    
+        expect(res.status).toBe(200);
+        expect(res.body.message).toBe('Task deleted');
+    });
 })
