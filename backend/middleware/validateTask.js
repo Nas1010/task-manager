8b1b199b7be2d2512166de1649b8c04d9e81ac20
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-04-08 17:31:16 +0100

    Updated logic in validateTask.js so it handles PUT request as required

--- backend/middleware/validateTask.js
+++ backend/middleware/validateTask.js
@@ -1,20 +1,21 @@
-const validateTask = (req, res, next) => {
-    const { title, description, status, due_date } = req.body;
-  
-    if (!title || !status) {
-      return res.status(400).json({ message: 'Title and status are required' });
+  const validateTask = (req, res, next) => {
+  const { title, description, status, due_date } = req.body;
+
+  if (req.method === 'POST' || req.method === 'PUT') {
+    if (!status) {
+      return res.status(400).json({ message: 'Status is required' });
     }
-  
-    if (due_date && isNaN(Date.parse(due_date))) {
-      return res.status(400).json({ message: 'Due date is not valid' });
-    }
-  
-    if (status && !['pending', 'in-progress', 'completed'].includes(status)) {
-      return res.status(400).json({ message: 'Status must be one of: pending, in-progress, completed' });
-    }
-  
-    next();
-  };
-  
-  export default validateTask;
-  
+  }
+
+  if (due_date && isNaN(Date.parse(due_date))) {
+    return res.status(400).json({ message: 'Due date is not valid' });
+  }
+
+  if (status && !['pending', 'in-progress', 'completed'].includes(status)) {
+    return res.status(400).json({ message: 'Status must be one of: pending, in-progress, completed' });
+  }
+
+  next();
+};
+
+export default validateTask;
